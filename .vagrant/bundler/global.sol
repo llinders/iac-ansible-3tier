ce7342cf09d68f405bf1f247519eb56eb2fc865d
Luc Linders <s1171479@student.windesheim.nl>
2023-03-07 14:12:05 +0100

    Removed general vagrant files from repository

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["racc",["~> 1.4"]],["nokogiri",["~> 1.6"]],["iniparse",["> 1.0"]],["log4r",["~> 1.1"]],["concurrent-ruby",["~> 1.0"]],["i18n",["~> 1.8"]],["vagrant-vmware-esxi",["= 2.5.5"]]],"checksum":"fac01806ad7b167e24132bcfd396158649421f1f035f7d39c86339324bdecb19","vagrant_version":"2.3.4"}
