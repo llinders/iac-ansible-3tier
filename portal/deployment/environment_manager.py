007d9e6a596eaca9598498eb198b0349157963ea
Luc Linders <s1171479@student.windesheim.nl>
2023-04-28 21:24:42 +0200

    start on deploy test environment functionality

--- portal/deployment/environment_manager.py
+++ portal/deployment/environment_manager.py
@@ -1,15 +1,30 @@
-def deploy_new_test_environment():
+import os
+import shutil
+from data.customer_data_utils import environment_deployed
+
+
+def deploy_new_test_environment(customer_number):
+    if environment_deployed('test', customer_number) == False:
+        newdir = './deployments/customer_%d/test'%customer_number
+        if not os.path.exists(newdir):
+            os.makedirs(newdir)
+            shutil.copy('./templates/Vagrantfile_testenv.j2', newdir + '/Vagrantfile.j2')
+            shutil.copy('./base/playbook.yml', newdir + '/playbook.yml')
+
+
+            return
+    print('Test environment is already deployed, cannot deploy multiple test environments')
     return
 
-def deploy_new_prod_environment():
+def deploy_new_prod_environment(customer_number):
     # check if environment already exists
     return
 
-def modify_prod_environment():
+def modify_prod_environment(customer_number):
     return
 
-def delete_test_environment():
+def delete_test_environment(customer_number):
     return
 
-def delete_prod_enviroment():
+def delete_prod_enviroment(customer_number):
     return
---
+++ portal/deployment/j2_template_modifier.py
@@ -0,0 +1,22 @@
+from jinja2 import Template
+import data.ip_data_utils as cdi
+
+__VAGRANTFILE_TEMPLATE_DIR_TEST__ = './templates/Vagrantfile_testenv.j2'
+__VAGRANTFILE_TEMPLATE_DIR_PROD__ = './templates/Vagrantfile_prodenv.j2'
+
+def modify_vagrantfile_testenv():
+    with open(__VAGRANTFILE_TEMPLATE_DIR_TEST__, 'r') as in_file:
+        template = Template(in_file.read())
+
+    ip_list: list = cdi.find_next_available_ips(2)
+
+    context = {
+        "webserver_ip": ip_list[0],
+        "database_ip": ip_list[1]
+    }
+
+    return template.render(context)
+        
+
+def modify_vagrantfile_prodenv():
+    return
