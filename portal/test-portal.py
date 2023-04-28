1fbd043f7921a755cea9da0ba2a883bae0dffbf5
Luc Linders <s1171479@student.windesheim.nl>
2023-04-28 21:25:37 +0200

    small changes

--- portal/test-portal.py
+++ portal/test-portal.py
@@ -1,8 +1,10 @@
 from menu.menu_navigation import Menu, show_menu
 import data.customer_data_utils as cdu
+import data.ip_data_utils as idu
+from deployment.j2_template_modifier import modify_vagrantfile_testenv
 
 ## Loading all customer data ##
-print(cdu._load_all_customer_data())
+#print(cdu._load_all_customer_data())
 
 ## Adding a customer to the file ##
 # portal.add_customer_to_file('test')
@@ -20,4 +22,17 @@ for customer in customers:
         print(customer)
     
 # Show existing customer menu
-show_menu(Menu.LOGIN)
+#show_menu(Menu.LOGIN)
+
+# Test environment_deployed function
+print(cdu.environment_deployed('test', 1))
+print(cdu.environment_deployed('test', 2))
+
+## Test ip_data_utils ##
+# find_next_available_ip()
+print(idu.find_next_available_ips(3))
+
+
+## j2_template_modifier ##
+# test env
+print(modify_vagrantfile_testenv())
