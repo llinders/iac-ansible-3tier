d06109bbf77511f9d3d5ccc881776467348782b8
Luc Linders <s1171479@student.windesheim.nl>
2023-04-28 21:23:10 +0200

    ip net switch

--- portal/data/generate_available_ip_addresses.py
+++ portal/data/generate_available_ip_addresses.py
@@ -1,12 +1,12 @@
 import json
 
 addresses = []
-for i in range(2, 255):
-    addresses.append("192.168.1." + str(i))
+for i in range(10, 200):
+    addresses.append("10.0.0." + str(i))
 
 dict = {
     "available_ip_addresses": addresses
 }
 
-with open('portal/available_ip_addresses.json', 'w') as outfile:
+with open('portal/data/available_ip_addresses.json', 'w') as outfile:
     json.dump(dict, outfile, indent=4)
